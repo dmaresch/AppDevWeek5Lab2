c848ff5369272bac52532b3d20684b86d7d1a911
dmaresch <dmaresch@my.wctc.edu>
2020-05-11 22:53:02 -0500

    commito juan

---
+++ README.md
@@ -0,0 +1,1 @@
+# AppDevWeek5Lab2
---
+++ W5LabB/W5LabB/ClockViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using System.Text;
+
+namespace W5LabB
+{
+    class ClockViewModel : INotifyPropertyChanged
+    {
+        DateTime dateTime;
+
+        int seconds;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ClockViewModel()
+        {
+            this.DateTime = DateTime.Now;
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                this.DateTime = DateTime.Now;
+                return true;
+            });
+        }
+
+        public DateTime DateTime
+        {
+            set
+            {
+                if (dateTime != value)
+                {
+                    dateTime = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("DateTime"));
+                    }
+                }
+            }
+            get
+            {
+                return dateTime;
+            }
+        }
+    }
+}
